ef2c6de8af4be93183e09c08f142aa8423db5bb9
EdGabrielOliveira <edgabrieloliveira@proton.me>
2025-02-11 09:47:51 -0300

    fix: troncando os links de navegação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
+import Link from "next/link";
 import React from "react";
 
 export default function page() {
   return (
     <div className="flex flex-col justify-center items-center text-center gap-4 pt-4">
-      <h1>Dev Buttons</h1>
-      <div className="flex flex-col">
-        <a href="landing">Landing PAGE</a>
-        <a href="auth/admin">Feed PAGE</a>
-        <a href="auth/login">Login PAGE</a>
-        <a href="auth/register">Register PAGE</a>
+      <h1>Painel do Desenvolvedor</h1>
+      <div className="flex flex-col gap-2">
+        <Link href="/landing/" target="_blank" className="underline">
+          Ir para Landing
+        </Link>
+        <Link href="/auth/admin/" target="_blank" className="underline">
+          Ir para Feed
+        </Link>
+        <Link href="/auth/login/" target="_blank" className="underline">
+          Ir para Login
+        </Link>
+        <Link href="/auth/register/" target="_blank" className="underline">
+          Ir para Register
+        </Link>
       </div>
     </div>
   );
